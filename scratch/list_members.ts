b70c5ff56c7cb32e21f709fd339c71b61685b0ab
Salim <msali@syrian-nl.org>
2026-06-21 13:13:34 +0200

    feat: add utility scripts to inspect board member data from primary database and backup files

--- scratch/list_members.ts
+++ scratch/list_members.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "../src/lib/db";
 
 async function main() {
   const members = await prisma.boardMember.findMany({
