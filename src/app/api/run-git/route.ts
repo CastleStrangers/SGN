f287b878e726f3ab9684ef07816ade981cd91574
Salim <msali@syrian-nl.org>
2026-06-21 12:47:44 +0200

    feat: add API route to fetch prisma seed content from specific git commit

--- src/app/api/run-git/route.ts
+++ src/app/api/run-git/route.ts
@@ -5,12 +5,17 @@ export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
-    const output1 = execSync("git --no-pager show ac0a210", { encoding: "utf8", timeout: 8000 });
+    const output1 = execSync("git --no-pager show 943bc9a:prisma/seed.cjs", { encoding: "utf8", timeout: 8000 });
     return NextResponse.json({ output1 });
   } catch (error: any) {
     return NextResponse.json({ error: error.message, stderr: error.stderr?.toString() });
   }
 }
+
+
+
+
+
 
 
 
