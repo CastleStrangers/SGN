3b1333275b84e8fc3ab4b8dca71e45ee7ca4e86f
Salim <msali@syrian-nl.org>
2026-06-29 11:18:41 +0200

    feat: add script to automate FACEBOOK_PAGE_TOKEN injection via Vercel CLI

--- scratch/add_fb_token.js
+++ scratch/add_fb_token.js
@@ -1,25 +1,26 @@
 const { spawn } = require('child_process');
 const fs = require('fs');
 
-fs.writeFileSync('vercel-add-log.txt', '');
+const logFile = 'vercel-add-log-direct.txt';
+fs.writeFileSync(logFile, '');
 
 const cp = spawn('npx.cmd', ['vercel', 'env', 'add', 'FACEBOOK_PAGE_TOKEN', 'production'], { shell: true });
 
 cp.stdout.on('data', d => {
   const str = d.toString();
-  fs.appendFileSync('vercel-add-log.txt', 'STDOUT: ' + str + '\n');
+  fs.appendFileSync(logFile, 'STDOUT: ' + str + '\n');
   
-  if (str.includes("value of FACEBOOK_PAGE_TOKEN")) {
-    fs.appendFileSync('vercel-add-log.txt', 'Writing token value...\n');
+  if (str.includes("value of FACEBOOK_PAGE_TOKEN") || str.includes("value")) {
+    fs.appendFileSync(logFile, 'Writing token value...\n');
     cp.stdin.write('EAAOCF9nXIWQBR1meARrMlLoDKmAZCS4VIeQ52Atrr319YAWcKLMzgXdxAGj17gUdGsOqZCp0tSmAxv1AcpYVLBZCj9nrGAOZAEHpXLCqpwXVH1JALfKnhHHhteawXZCtL3vf3CB2VLl2ZBVNgeJFTjCm0ak1CaN2ZAhLUBsG7ZByACmr3cd6JnDEo4B9tzwn82c1Js4GiCxSKkoT7iMVc5Q2T1VNURW8KJjzZCOOSdbFFGWEpl8XsLoyTBegZD\n');
   }
 });
 
 cp.stderr.on('data', d => {
-  fs.appendFileSync('vercel-add-log.txt', 'STDERR: ' + d.toString() + '\n');
+  fs.appendFileSync(logFile, 'STDERR: ' + d.toString() + '\n');
 });
 
 setTimeout(() => {
   cp.kill();
-  fs.appendFileSync('vercel-add-log.txt', 'FINISHED\n');
+  fs.appendFileSync(logFile, 'FINISHED\n');
 }, 6000);
