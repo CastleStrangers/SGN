d3365262a2f3e510a0dfd4a6e361b3a4242568ec
Salim <msali@syrian-nl.org>
2026-07-01 03:58:14 +0200

    feat: add temporary API route to execute and return git status information

--- src/app/api/temp-run-specs/route.ts
+++ src/app/api/temp-run-specs/route.ts
@@ -3,7 +3,7 @@ import { exec } from "child_process";
 
 export async function GET() {
   return new Promise((resolve) => {
-    exec("git diff", (error, stdout, stderr) => {
+    exec("git status -u", (error, stdout, stderr) => {
       resolve(NextResponse.json({ error, stdout, stderr }));
     });
   });
