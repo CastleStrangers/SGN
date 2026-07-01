9effc5ff7e7ab09fbe7f2d9a852d3dc8643dbaab
Salim <msali@syrian-nl.org>
2026-07-01 03:58:28 +0200

    feat: add temporary API route to execute git log commands

--- src/app/api/temp-run-specs/route.ts
+++ src/app/api/temp-run-specs/route.ts
@@ -3,7 +3,7 @@ import { exec } from "child_process";
 
 export async function GET() {
   return new Promise((resolve) => {
-    exec("git status -u", (error, stdout, stderr) => {
+    exec("git log -n 5", (error, stdout, stderr) => {
       resolve(NextResponse.json({ error, stdout, stderr }));
     });
   });
