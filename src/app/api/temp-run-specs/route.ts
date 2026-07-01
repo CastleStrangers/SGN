77c75690cc072a3f895963498fab4f95f2d6a15b
Salim <msali@syrian-nl.org>
2026-07-01 03:58:42 +0200

    feat: add API route to retrieve recent git commit history

--- src/app/api/temp-run-specs/route.ts
+++ src/app/api/temp-run-specs/route.ts
@@ -3,7 +3,7 @@ import { exec } from "child_process";
 
 export async function GET() {
   return new Promise((resolve) => {
-    exec("git log -n 5", (error, stdout, stderr) => {
+    exec("git log -n 20 --oneline", (error, stdout, stderr) => {
       resolve(NextResponse.json({ error, stdout, stderr }));
     });
   });
